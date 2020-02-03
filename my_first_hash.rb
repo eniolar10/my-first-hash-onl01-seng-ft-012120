d4dcbc7494ee273b7a03dc44d324ed0f131a4a93
Ola Enol <eniolar10@gmail.com>
2020-02-03 18:19:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,7 +30,7 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3,"muskets"=> 2,"gun powder" =>4
+                  "oil paintings" => 3
                  }
                
          shipping_manifest
